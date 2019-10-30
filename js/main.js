c1d020f79b0492d8f6b2ac7b52289fcec3d1d0e2
Nipun Nirmana <nipunfernando@gmail.com>
2019-10-30 16:52:12 +0530

    Basic code added for user cards

--- js/main.js
+++ js/main.js
@@ -1,5 +1,97 @@
-window.onload = () => {
-  "use strict";
+const url = "./js/user.js";
+fetch(url)
+  .then(resp => resp.json())
+  .then(function(data) {
+    data.users.forEach(user => {
+      let { age, phone, address, category } = user;
+      const listData = [
+        { name: "age", value: escapeSpaces(age) },
+        { name: "phone", value: escapeSpaces(phone) },
+        { name: "address", value: escapeSpaces(address) },
+        { name: "category", value: escapeSpaces(category) }
+      ];
+      document.querySelector("#test").insertAdjacentHTML(
+        "beforeend",
+        `<my-card
+          title="${user.firstName} ${user.lastName}"
+          img = ${user.picture}
+          list-info = ${JSON.stringify(listData)}
+           />`
+      );
+    });
+  })
+  .catch(function(error) {
+    console.error("Error Fetching User Data", error);
+  });
 
-  console.log("TEST");
-};
+const escapeSpaces = str =>
+  str
+    .toString()
+    .split(" ")
+    .join("*--");
+
+const unEscapeSpaces = str =>
+  str
+    .toString()
+    .split("*--")
+    .join(" ");
+
+class MyCard extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  getAttrVal(attr) {
+    return this.getAttribute(attr) || "N/A";
+  }
+
+  getListData(items) {
+    return JSON.parse(items)
+      .map(item => {
+        let { name, value } = item;
+        return `<div class="info-wrapper">
+      <div class="name">${unEscapeSpaces(name)}</div>
+      <div class="value">${unEscapeSpaces(value)}</div>
+     </div>`;
+      })
+      .join("");
+  }
+
+  connectedCallback() {
+    const htmlContent = `<div class="card user">
+    <div class="card-image"><img src="${this.getAttrVal("img")}"></div>
+    <div class="card-details">
+    <div class="card-title">${this.getAttrVal("title")}</div>
+    ${this.getListData(this.getAttrVal("list-info"))}
+    
+      </div>
+    </div>
+  </div>`;
+
+    this.innerHTML = htmlContent;
+  }
+
+  disconnectedCallback() {
+    // browser calls this method when the element is removed from the document
+    // (can be called many times if an element is repeatedly added/removed)
+  }
+
+  static get observedAttributes() {
+    return [
+      /* array of attribute names to monitor for changes */
+    ];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    // called when one of attributes listed above is modified
+  }
+
+  adoptedCallback() {
+    // called when the element is moved to a new document
+    // (happens in document.adoptNode, very rarely used)
+  }
+
+  // there can be other element methods and properties
+}
+
+customElements.define("my-card", MyCard);
